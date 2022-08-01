dd2a43e798089e544b8b318364511099c32723c1
Lukas Bildstein <lukas.bildstein@outlook.com>
2022-08-01 12:48:16 +0200

    Added polarization table and note table to alice page

--- qkd-game-client/src/pages/GameAlice.tsx
+++ qkd-game-client/src/pages/GameAlice.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import NoteTable from '../components/game/NoteTable';
+import PolarizationTable from '../components/game/PolarizationTable';
 import Sender from '../components/game/Sender';
 import Nav from '../components/Nav';
 import WidthLimiter from '../components/WidthLimiter';
@@ -22,12 +23,25 @@ function GameAlice() {
         <React.Fragment>
             <Nav></Nav>
             <WidthLimiter>
-                <Sender
-                    onPolarizedPhotonTransported={
-                        handlePolarizedPhotonTransported
-                    }
-                />
-                <NoteTable noOfQubits={10}></NoteTable>
+                <div className="flex justify-between">
+                    <div className="flex-none">
+                        <Sender
+                            onPolarizedPhotonTransported={
+                                handlePolarizedPhotonTransported
+                            }
+                        />
+                    </div>
+                    <div className="flex flex-1 justify-between ml-6 w-full min-w-0 items-start">
+                        <div className="flex-initial mr-6 w-full min-w-0">
+                            <div className="flex overflow-x-auto overflow-y-hidden pt-11 pb-20 pl-2 pr-20 border-2 shadow-inner">
+                                <NoteTable noOfQubits={20}></NoteTable>
+                            </div>
+                        </div>
+                        <div className="flex-none py-10">
+                            <PolarizationTable></PolarizationTable>
+                        </div>
+                    </div>
+                </div>
             </WidthLimiter>
         </React.Fragment>
     );
