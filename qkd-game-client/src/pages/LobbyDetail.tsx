7c29ad70af4a5dd9b368abaf18a92e904c5167c9
Lukas Bildstein <lukas.bildstein@outlook.com>
2022-08-08 15:28:38 +0200

    Re-styled lobby detail page

--- qkd-game-client/src/pages/LobbyDetail.tsx
+++ qkd-game-client/src/pages/LobbyDetail.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Button from '../components/Button';
+import MessageLog from '../components/MessageLog';
 import Nav from '../components/Nav';
 import WidthLimiter from '../components/WidthLimiter';
+import AuthStorage from '../helper/AuthStorage';
 import { useSocket } from '../helper/IO';
 import IGameJson from '../models/api/IGameJson';
 import ILobbyJson from '../models/api/ILobbyJson';
@@ -12,20 +14,15 @@ import Lobby from '../models/Lobby';
 import LobbiesService from '../services/LobbyService';
 
 function LobbyDetail() {
+    const authStorage = new AuthStorage();
+    const loggedInUser = authStorage.getLoggedInUser();
     const params = useParams();
     const socket = useSocket();
     const lobbyId = params.lobbyId;
     const lobbiesService = new LobbiesService();
     const navigate = useNavigate();
     const [lobby, setLobby] = useState<Lobby>();
-    const [chatText, setChatText] = useState('');
-    // Ref necessary because we want to register event handlers only once, but have a dependecy to chatText.
-    // Adding chatText as a dependency would repeatetly register the handler with every chatText update.
-    const chatTextRef = React.useRef(chatText);
-
-    useEffect(() => {
-        chatTextRef.current = chatText;
-    });
+    const [messages, setMessages] = useState<string[]>([]);
 
     useEffect(() => {
         loadLobby();
@@ -34,19 +31,19 @@ function LobbyDetail() {
     useEffect(() => {
         if (socket && lobby?.id) {
             socket.on('updatedLobby', updatedLobbyHandler);
-            socket.on('chatMessage', chatMessageHandler);
+            socket.on('chatMessage', appendMessage);
             socket.on('startedGame', startedGameHandler);
             socket.emit('joinLobby', lobby.id);
             return () => {
                 socket.off('updatedLobby', updatedLobbyHandler);
-                socket.off('chatMessage', chatMessageHandler);
+                socket.off('chatMessage', appendMessage);
                 socket.off('startedGame', startedGameHandler);
             };
         }
     }, [socket, lobby]);
 
-    function chatMessageHandler(message: string) {
-        setChatText(`${chatTextRef.current} ${message}`);
+    function appendMessage(message: string) {
+        setMessages((prevMessages) => [...prevMessages, message]);
     }
 
     function updatedLobbyHandler(lobbyJson: ILobbyJson) {
@@ -77,38 +74,11 @@ function LobbyDetail() {
         }
     }
 
-    return (
-        <React.Fragment>
-            <Nav></Nav>
-            <WidthLimiter>
-                <h1 className="text-3xl font-mono py-3">{lobby?.name}</h1>
-                <Button
-                    onClick={() => {
-                        selectLobbyRole(PLAYERROLE.alice);
-                    }}
-                    disabled={lobby?.reservedAlice ? true : false}
-                >
-                    {lobby?.reservedAlice
-                        ? lobby.selectedRole === PLAYERROLE.alice
-                            ? `You play as `
-                            : `${lobby.reservedAlice.name} plays as `
-                        : 'Select role: '}
-                    Alice
-                </Button>
-                <Button
-                    onClick={() => {
-                        selectLobbyRole(PLAYERROLE.bob);
-                    }}
-                    disabled={lobby?.reservedBob ? true : false}
-                >
-                    {lobby?.reservedBob
-                        ? lobby.selectedRole === PLAYERROLE.bob
-                            ? `You play as `
-                            : `${lobby.reservedBob.name} plays as `
-                        : 'Select role: '}
-                    Bob
-                </Button>
-                {/* TODO make a "ready" feature for the player that are not hosting the lobby. */}
+    function getButton() {
+        let button: JSX.Element = <div></div>;
+
+        if (lobby?.owner.id === loggedInUser?.id) {
+            button = (
                 <Button
                     onClick={() => {
                         if (lobby) {
@@ -118,7 +88,58 @@ function LobbyDetail() {
                 >
                     Start Game
                 </Button>
-                <span>{chatText}</span>
+            );
+        } else {
+            button = <Button disabled={true}>Wait for host to start</Button>;
+        }
+
+        return button;
+    }
+
+    return (
+        <React.Fragment>
+            <Nav></Nav>
+            <WidthLimiter>
+                <h1 className="text-3xl font-mono py-3">{lobby?.name}</h1>
+                <div className="flex flex-col items-stretch w-1/2">
+                    <div className="flex mb-6 items-center">
+                        <div className="w-1/3 flex-none">Select your role:</div>
+                        <div className="w-2/3 flex-none flex flex-col items-start">
+                            <Button
+                                className="mb-3"
+                                onClick={() => {
+                                    selectLobbyRole(PLAYERROLE.alice);
+                                }}
+                                disabled={lobby?.reservedAlice ? true : false}
+                            >
+                                {lobby?.reservedAlice
+                                    ? lobby.selectedRole === PLAYERROLE.alice
+                                        ? `You play as `
+                                        : `${lobby.reservedAlice.name} plays as `
+                                    : ''}
+                                Alice
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    selectLobbyRole(PLAYERROLE.bob);
+                                }}
+                                disabled={lobby?.reservedBob ? true : false}
+                            >
+                                {lobby?.reservedBob
+                                    ? lobby.selectedRole === PLAYERROLE.bob
+                                        ? `You play as `
+                                        : `${lobby.reservedBob.name} plays as `
+                                    : ''}
+                                Bob
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex mb-10">
+                        <div className="w-1/3 flex-none"></div>
+                        <div className="w-2/3 flex-none">{getButton()}</div>
+                    </div>
+                </div>
+                <MessageLog messages={[]}></MessageLog>
             </WidthLimiter>
         </React.Fragment>
     );
