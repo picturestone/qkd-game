7be209c9d52e984060388f88bc4f21f8189fbe34
Lukas Bildstein <lukas.bildstein@outlook.com>
2022-08-14 15:40:23 +0200

    Added input validation

--- routes/lobbiesRouter.ts
+++ routes/lobbiesRouter.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import LobbyDb from '../database/LobbyDb';
 import Lobby from '../models/Lobby';
 import ILobbyJson from '../qkd-game-client/src/models/api/ILobbyJson';
+import Validator from '../helper/Validator';
 
 const router = express.Router();
 const lobbyDb = new LobbyDb();
+const validator = new Validator();
 
 router.get('/', function (req, res) {
     const lobbyJson = new Array<ILobbyJson>();
@@ -22,18 +24,23 @@ router.get('/', function (req, res) {
 });
 
 router.get('/:id', function (req, res) {
-    lobbyDb
-        .findById(req.params.id)
-        .then((lobby) => {
-            if (lobby) {
-                res.send(lobby.toJson());
-            } else {
-                res.status(404).send();
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    const id = req.params.id;
+    if (validator.isId(id)) {
+        lobbyDb
+            .findById(id)
+            .then((lobby) => {
+                if (lobby) {
+                    res.send(lobby.toJson());
+                } else {
+                    res.status(404).send();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        res.status(400).send('invalid :id');
+    }
 });
 
 router.post('/', function (req, res) {
@@ -43,15 +50,19 @@ router.post('/', function (req, res) {
         noOfQbits: req.body.noOfQbits,
         isEveAllowed: req.body.isEveAllowed,
     };
-    const lobbyModel = Lobby.fromJson(lobbyJson);
-    lobbyDb
-        .create(lobbyModel)
-        .then((savedLobby) => {
-            res.status(201).send(savedLobby.toJson());
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    if (validator.isILobbyJson(lobbyJson)) {
+        const lobbyModel = Lobby.fromJson(lobbyJson);
+        lobbyDb
+            .create(lobbyModel)
+            .then((savedLobby) => {
+                res.status(201).send(savedLobby.toJson());
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        res.status(400).send('invalid lobby json');
+    }
 });
 
 export default router;
--- routes/usersRouter.ts
+++ routes/usersRouter.ts
@@ -3,18 +3,24 @@ import User from '../models/User';
 import UserDb from '../database/UserDb';
 import { generateAccessToken } from '../auth/jwt';
 import IUserJson from '../qkd-game-client/src/models/api/IUserJson';
+import Validator from '../helper/Validator';
 
 const router = express.Router();
 const userDb = new UserDb();
+const validator = new Validator();
 
 router.post('/', function (req, res) {
     const userJson: IUserJson = {
         name: req.body.name,
     };
-    const userModel = User.fromJson(userJson);
-    userDb.create(userModel).then((createdUser) => {
-        res.status(201).send(`Bearer ${generateAccessToken(createdUser)}`);
-    });
+    if (validator.isIUserJson(userJson)) {
+        const userModel = User.fromJson(userJson);
+        userDb.create(userModel).then((createdUser) => {
+            res.status(201).send(`Bearer ${generateAccessToken(createdUser)}`);
+        });
+    } else {
+        res.status(400).send('invalid user json');
+    }
 });
 
 export default router;
