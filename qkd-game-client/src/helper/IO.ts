1c695a55b03f3cb175bdf050aedab12500c78a95
Lukas Bildstein <lukas.bildstein@outlook.com>
2022-08-16 04:22:54 +0200

    Replaced singleton hook with simpler global property on window object

--- qkd-game-client/src/helper/IO.ts
+++ qkd-game-client/src/helper/IO.ts
@@ -1,31 +1,22 @@
-import { singletonHook } from 'react-singleton-hook';
 import { io, Socket } from 'socket.io-client';
 import IClientToServerEvents from '../models/api/IClientToServerEvents';
 import IServerToClientEvents from '../models/api/IServerToClientEvents';
 import AuthStorage from './AuthStorage';
 
-function getSocket() {
-    const socket: Socket<IServerToClientEvents, IClientToServerEvents> = io({
-        extraHeaders: {
-            authorization: new AuthStorage().getToken() || '',
-        },
-    });
-
-    function connectSocket() {
-        if (!socket.connected) {
-            console.log('trying to connect socket');
-            socket.connect();
-            setTimeout(() => {
-                connectSocket();
-            }, 5000);
-        }
+declare global {
+    interface Window {
+        socketIoInstance: Socket<IServerToClientEvents, IClientToServerEvents>;
     }
-
-    connectSocket();
-
-    return socket;
 }
 
-// TODO Singleton hook is not yet using the new render method of react 18.
-// See https://github.com/Light-Keeper/react-singleton-hook/issues/479
-export const useSocket = singletonHook(undefined, getSocket);
+export function useSocket() {
+    if (!window.socketIoInstance) {
+        window.socketIoInstance = io({
+            extraHeaders: {
+                authorization: new AuthStorage().getToken() || '',
+            },
+        });
+    }
+
+    return window.socketIoInstance;
+}
